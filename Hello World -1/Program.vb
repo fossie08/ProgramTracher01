daec4f838afaafd700ff0849dae8c309183e3605
Ollie Foster <00.aegis-aseries@icloud.com>
2021-12-03 21:33:11 +0000

    initial commit

---
+++ Hello World -1/Program.vb
@@ -0,0 +1,19 @@
+﻿'
+' Created by SharpDevelop.
+' User: 19ofoster
+' Date: 07/10/2021
+' Time: 11:18
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Module Program
+	Sub Main()
+		'Asks the user for an input
+		Console.WriteLine("Press any key . . ")
+		Console.ReadKey(True)
+		'Displays Hello World
+		Console.WriteLine("Hello World")
+		Console.WriteLine("Press any key to continue . . . ")
+		Console.ReadKey(True)
+	End Sub
+End Module
